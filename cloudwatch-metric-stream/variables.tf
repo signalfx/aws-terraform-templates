1680bb92b50c8ba5bb69a5702c5235976de94bf4
Aleksandra Konarska <86464734+akonarska@users.noreply.github.com>
2025-10-31 13:12:32 +0100

    Fix endpoint docs

--- cloudwatch-metric-stream/variables.tf
+++ cloudwatch-metric-stream/variables.tf
@@ -4,7 +4,7 @@ variable splunk_access_token {
 }
 
 variable splunk_ingest_url {
-  description = "Find the real-time data ingest URL in Profile > Account Settings > Endpoints. Note: do NOT include endpoint path here: for instance use https://ingest.us1.signalfx.com instead of https://ingest.us1.signalfx.com/v1/cloudwatch_metric_stream."
+  description = "Copy the Real-time Data Ingest Endpoint value from My Profile > Organizations."
   type = string
 }
 variable "AWS_REGION" {
